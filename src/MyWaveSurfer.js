0207a3bb477501b345ecbe2fa4efdb27c3c9af73
Twinshu <twinshup@gmail.com>
2023-11-08 23:50:25 +0530

    add: waveform added

---
+++ src/MyWaveSurfer.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react';
+import WaveSurfer from 'wavesurfer.js';
+import './style.css'
+const MyWaveSurfer = ({ audioSrc }) => {
+  const waveformRef = useRef(null);
+
+  useEffect(() => {
+    const wavesurfer = WaveSurfer.create({
+      container: waveformRef.current,
+      backend: 'WebAudio',
+      responsive: true,
+      waveColor: 'blue',
+      progressColor: 'purple',
+    });
+
+    wavesurfer.load(audioSrc);
+
+    return () => {
+      wavesurfer.destroy();
+    };
+  }, [audioSrc]);
+
+  return <div className='waveform' ref={waveformRef} />;
+};
+
+export default MyWaveSurfer;
